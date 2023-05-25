fb671caecc3c8d7d8928e1bfd563e8bd9d8b0d31
DennisKemboi <cherogonydennis9@gmail.com>
2023-05-25 18:48:35 +0300

    Completed Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,26 @@
-class Student 
+class Student < User
+
+    def initialize()
+        @knowledge = []
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+    def learn(strong)
+        @knowledge.push(strong)
+    end
+
+    def first_name
+        @@first_name
+      end
+    
+      def last_name
+        @@last_name
+      end
 
 end
+
+s1 = Student.new
+s1
---
+++ lib/user.rb
@@ -0,0 +1,22 @@
+class User
+
+    # def initialize(name)
+    # end
+
+    def first_name= (name1)
+        @@first_name =name1
+    end
+
+    def first_name
+        @@first_name
+    end
+
+    def last_name= (name2)
+        @@last_name = name2
+    end
+
+    def last_name
+        @@last_name
+    end
+
+end
